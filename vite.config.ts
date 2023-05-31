f0329b35f032b96aa7d525f4948d4464fbe735ee
shimonevt <semenevtuhov@gmail.com>
2023-05-31 09:21:37 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+process.env.GQL_SERVER_URL = 'https://api-stage.santiment.net/graphql';
+process.env.IS_DEV_MODE = process.env.NODE_ENV !== 'production';
+process.env.MEDIA_PATH = '/node_modules/san-webkit/lib';
+process.env.ICONS_PATH = '/node_modules/san-webkit/lib/icons';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	define: {
+		'process.env.IS_DEV_MODE': process.env.IS_DEV_MODE
+	}
+});
