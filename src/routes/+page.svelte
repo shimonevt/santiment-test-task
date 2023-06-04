84334e47e539b111d35f564bf8bce31d80151b7d
shimonevt <semenevtuhov@gmail.com>
2023-06-04 20:14:44 +0300

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,59 @@
-<script>
+<script lang="ts">
+	import { showEditorDialog } from '@/components/ProjectEditor/index.svelte';
+	import 'webkit/styles/main.css';
+
+	import { getAllProjects } from '@/api';
+	import { onMount } from 'svelte';
+	import ProjectList from '../components/ProjectList/index.svelte';
+	import type { Project as ProjectType } from '@/types/project';
+	import type { ChangeProjectEvent } from '@/types/events';
+
+	import { projectsStore } from '@/store';
+
+	let windows: any[] = [];
+	let listItems: ProjectType[];
+	let displayedItems: ProjectType[];
+	let isDialogOpened: boolean | undefined;
+	let queryString: string = '';
+
+	$: displayedItems =
+		queryString.length !== 0
+			? (displayedItems = listItems.filter(filterByName))
+			: (displayedItems = listItems);
+
+	projectsStore.subscribe((value) => {
+		listItems = value.selectedItems;
+		isDialogOpened = value.isDialogOpened;
+	});
+
+	onMount(async () => {
+		await getAllProjects().then((projects) => {
+			if (projects) {
+				projectsStore.setInitialItems(projects);
+			}
+		});
+	});
+
+	function filterByName(item: ProjectType) {
+		return item.name.toLowerCase().includes(queryString);
+	}
+
+	function handleChangeSave(ev: CustomEvent<ChangeProjectEvent[]>) {
+		projectsStore.setSelectedItems(ev.detail);
+	}
+
+	function onToggle() {
+		showEditorDialog({ strict: true, saveChanges: handleChangeSave }).then((window) => {
+			windows.push(window);
+			windows = windows;
+		});
+	}
+
+	function handleInputChange(ev: Event) {
+		if (ev && typeof (ev.target as HTMLInputElement)?.value !== 'undefined') {
+			queryString = (ev.target as HTMLInputElement).value;
+		}
+	}
 </script>
 
 <svelte:head>
@@ -6,34 +61,11 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section />
+<main>
+	<div>
+		<ProjectList projects={displayedItems} on:toggleList={onToggle} on:input={handleInputChange} />
+	</div>
+</main>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+<style lang="scss">
 </style>
